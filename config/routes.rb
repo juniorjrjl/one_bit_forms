4ee7a678b0cf1cd5a565d22d05a277e70398d4aa
Junior <junior.jr.jl@gmail.com>
2021-01-18 20:14:45 -0300

    criação dos models e controllers e configuração de routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+      resources :forms
+      resources :questions, only: %i[create update destroy]
+      resources :answers, only: %i[index show create destroy]
     end
   end
 end
